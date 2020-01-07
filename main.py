5dc17e2a43b7b6b68d9ade11c77aea6cc17b8fb2
Peter Arnold <peter@deepsixdesign.com>
2020-01-07 17:08:58 +0000

    asd

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ client = storage.Client()
 
 def make_thumbnail(data, context):
     # Get the file that has been uploaded to GCS
-    bucket = client.get_bucket(data['gs://plexus-1d216-media-raw'])
+    bucket = client.get_bucket(data['plexus-1d216-media-raw'])
     blob = bucket.get_blob(data['name'])
     imagedata = blob.download_as_string()
     # Create a new image object and resample it
     newimage = Image(blob=imagedata)
     newimage.sample(200, 200)
     # Upload the resampled image to the other bucket
-    bucket = client.get_bucket('gs://plexus-1d216-media-processed')
+    bucket = client.get_bucket('plexus-1d216-media-processed')
     newblob = bucket.blob('thumbnail-' + data['name'])
     newblob.upload_from_string(newimage.make_blob())
